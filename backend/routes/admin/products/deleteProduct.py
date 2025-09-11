b454f7d213c6436fb145400ec8eb2992042b1a63
Jaymar Roco <131925937+MosquitoCoil@users.noreply.github.com>
2025-09-11 14:35:00 +0800

    Landing page update
    change the interface of the landing page

--- backend/routes/admin/products/deleteProduct.py
+++ backend/routes/admin/products/deleteProduct.py
@@ -2,19 +2,26 @@ from flask import Blueprint, redirect, url_for, flash
 from ....utils.decorators import role_required
 from database.database import get_db_connection
 
-deleteProduct_bp = Blueprint('deleteProduct', __name__)
+deleteProduct_bp = Blueprint("deleteProduct", __name__)
 
-@deleteProduct_bp.route('/delete-product/<int:product_id>', methods=['POST'])
-@role_required('admin')
+
+@deleteProduct_bp.route("/delete-product/<int:product_id>", methods=["POST"])
+@role_required("admin")
 def deleteProduct(product_id):
-
     try:
         conn = get_db_connection()
         cursor = conn.cursor()
+
+        # First, remove references from cart
+        cursor.execute("DELETE FROM cart WHERE product_id = %s", (product_id,))
+
+        # Then, delete the product
         cursor.execute("DELETE FROM products WHERE id = %s", (product_id,))
+
         conn.commit()
         cursor.close()
-        flash(f"Product deleted successfully.","success")
+        flash(f"Product deleted successfully.", "success")
     except Exception as e:
         flash(f"Error deleting Product: {str(e)}", "error")
+
     return redirect(url_for("productList.productList"))
--- backend/routes/home.py
+++ backend/routes/home.py
@@ -1,5 +1,4 @@
-from flask import Blueprint, render_template
-from werkzeug.utils import secure_filename
+from flask import Blueprint, render_template, request
 from database.database import get_db_connection
 
 home_bp = Blueprint("home", __name__, template_folder="../../frontend/templates/admin")
@@ -7,10 +6,11 @@ home_bp = Blueprint("home", __name__, template_folder="../../frontend/templates/
 
 @home_bp.route("/")
 def home():
+    view_all = request.args.get("view_all", default=0, type=int)
     conn = get_db_connection()
     cursor = conn.cursor(dictionary=True)
     cursor.execute("SELECT * FROM products")
     products = cursor.fetchall()
     cursor.close()
     conn.close()
-    return render_template("home.html", products=products)
+    return render_template("home.html", products=products, view_all=view_all)
