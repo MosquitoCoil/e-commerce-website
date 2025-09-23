0cfe80cb93106f17920b8ac51c2ced4a65b199f1
Jaymar Roco <131925937+MosquitoCoil@users.noreply.github.com>
2025-09-23 11:49:53 +0800

    Prepare for deployment

--- database/database.py
+++ database/database.py
@@ -1,15 +1,16 @@
+import os
 import mysql.connector
 
 db_config = {
-    "host": "localhost",
-    "user": "root",
-    "password": "",
-    "database": "e-commerce-web",
+    "host": os.getenv("DB_HOST", "localhost"),
+    "user": os.getenv("DB_USER", "root"),
+    "password": os.getenv("DB_PASSWORD", ""),
+    "database": os.getenv("DB_NAME", "e-commerce-web"),
 }
 
 
 def get_db_connection():
     conn = mysql.connector.connect(**db_config)
-    # force dict cursor every time
+    # Always return dictionary-style results
     conn.cursor_factory = lambda: conn.cursor(dictionary=True)
     return conn
