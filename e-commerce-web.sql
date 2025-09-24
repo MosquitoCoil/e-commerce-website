29764a9d2eb2f14f3cb2668f982ca4d087c71d5f
Jaymar Roco <131925937+MosquitoCoil@users.noreply.github.com>
2025-09-24 15:41:41 +0800

    Update e-commerce-web.sql

--- e-commerce-web.sql
+++ e-commerce-web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 22, 2025 at 09:40 AM
+-- Generation Time: Sep 24, 2025 at 09:40 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -42,9 +42,7 @@ CREATE TABLE `cart` (
 
 INSERT INTO `cart` (`id`, `user_id`, `product_id`, `quantity`, `created_at`, `size`) VALUES
 (171, 17, 23, 1, '2025-09-22 04:56:28', 'L'),
-(177, 19, 28, 1, '2025-09-22 05:59:49', 'M'),
-(178, 19, 26, 1, '2025-09-22 05:59:55', 'L'),
-(179, 19, 30, 1, '2025-09-22 06:00:02', 'XL');
+(181, 2, 24, 1, '2025-09-23 04:11:56', 'L');
 
 -- --------------------------------------------------------
 
@@ -75,7 +73,7 @@ INSERT INTO `orders` (`id`, `user_id`, `total`, `status`, `created_at`, `receive
 (31, 2, 2945.00, 'Pending', '2025-09-01 10:39:35', NULL),
 (32, 18, 3394.00, 'Completed', '2025-09-01 10:44:23', NULL),
 (34, 18, 3394.00, 'Received', '2025-09-01 10:47:43', NULL),
-(36, 18, 2346.00, 'Pending', '2025-09-01 10:53:07', NULL),
+(36, 18, 2346.00, 'Processing', '2025-09-01 10:53:07', NULL),
 (39, 17, 1358.00, 'Pending', '2025-09-19 07:43:51', NULL),
 (42, 17, 1295.00, 'Pending', '2025-09-19 09:39:31', NULL),
 (43, 17, 998.00, 'Pending', '2025-09-20 01:10:15', NULL),
@@ -84,7 +82,11 @@ INSERT INTO `orders` (`id`, `user_id`, `total`, `status`, `created_at`, `receive
 (46, 17, 499.00, 'Pending', '2025-09-20 01:33:44', NULL),
 (49, 17, 499.00, 'Received', '2025-09-20 01:38:10', NULL),
 (50, 19, 998.00, 'Received', '2025-09-22 05:38:09', NULL),
-(51, 19, 1996.00, 'Pending', '2025-09-22 05:59:33', NULL);
+(52, 2, 499.00, 'Completed', '2025-09-23 04:11:10', NULL),
+(57, 19, 1497.00, 'Cancelled', '2025-09-24 03:41:32', NULL),
+(62, 19, 998.00, 'Pending', '2025-09-24 06:22:17', NULL),
+(66, 19, 1497.00, 'Pending', '2025-09-24 06:29:45', NULL),
+(67, 19, 1497.00, 'Received', '2025-09-24 06:39:05', NULL);
 
 -- --------------------------------------------------------
 
@@ -96,6 +98,7 @@ CREATE TABLE `order_items` (
   `id` int(11) NOT NULL,
   `order_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
+  `size` varchar(50) DEFAULT NULL,
   `quantity` int(11) NOT NULL,
   `price` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -104,12 +107,19 @@ CREATE TABLE `order_items` (
 -- Dumping data for table `order_items`
 --
 
-INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `quantity`, `price`) VALUES
-(155, 50, 23, 1, 499.00),
-(156, 50, 24, 1, 499.00),
-(157, 51, 27, 1, 499.00),
-(158, 51, 21, 2, 499.00),
-(159, 51, 33, 1, 499.00);
+INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `size`, `quantity`, `price`) VALUES
+(155, 50, 23, NULL, 1, 499.00),
+(156, 50, 24, NULL, 1, 499.00),
+(160, 52, 24, NULL, 1, 499.00),
+(171, 57, 26, NULL, 1, 499.00),
+(172, 57, 30, NULL, 1, 499.00),
+(173, 57, 22, NULL, 1, 499.00),
+(187, 62, 27, NULL, 1, 499.00),
+(188, 62, 21, NULL, 1, 499.00),
+(192, 66, 23, 'M', 2, 499.00),
+(193, 66, 27, 'XXL', 1, 499.00),
+(194, 67, 27, 'L', 2, 499.00),
+(195, 67, 22, 'M', 1, 499.00);
 
 -- --------------------------------------------------------
 
@@ -163,24 +173,24 @@ CREATE TABLE `product_variants` (
 
 INSERT INTO `product_variants` (`id`, `product_id`, `size`, `stock`) VALUES
 (13, 21, 'S', 12),
-(14, 21, 'M', 9),
+(14, 21, 'M', 8),
 (15, 21, 'L', 15),
 (16, 21, 'XL', 14),
 (17, 21, 'XXL', 15),
 (18, 22, 'S', 12),
-(19, 22, 'M', 10),
-(20, 22, 'L', 16),
+(19, 22, 'M', 9),
+(20, 22, 'L', 15),
 (21, 22, 'XL', 12),
 (22, 22, 'XXL', 15),
 (23, 23, 'S', 10),
-(24, 23, 'M', 10),
+(24, 23, 'M', 7),
 (25, 23, 'L', 10),
 (26, 23, 'XL', 13),
 (27, 23, 'XXL', 14),
 (28, 24, 'S', 10),
 (29, 24, 'M', 10),
-(30, 24, 'L', 12),
-(31, 24, 'XL', 13),
+(30, 24, 'L', 10),
+(31, 24, 'XL', 12),
 (32, 24, 'XXL', 14),
 (33, 25, 'S', 10),
 (34, 25, 'M', 11),
@@ -193,10 +203,10 @@ INSERT INTO `product_variants` (`id`, `product_id`, `size`, `stock`) VALUES
 (41, 26, 'XL', 13),
 (42, 26, 'XXL', 14),
 (43, 27, 'S', 10),
-(44, 27, 'M', 10),
-(45, 27, 'L', 12),
+(44, 27, 'M', 9),
+(45, 27, 'L', 9),
 (46, 27, 'XL', 11),
-(47, 27, 'XXL', 14),
+(47, 27, 'XXL', 13),
 (48, 28, 'S', 10),
 (49, 28, 'M', 10),
 (50, 28, 'L', 12),
@@ -251,7 +261,10 @@ INSERT INTO `purchase_history` (`id`, `user_id`, `order_id`, `product_id`, `prod
 (3, 18, 34, NULL, 'White Shirt', 2, 499.00, 998.00, '2025-09-02 08:59:14'),
 (4, 18, 34, NULL, 'Goku Black Shirt', 1, 699.00, 699.00, '2025-09-02 08:59:14'),
 (5, 19, 50, 23, 'Yellow Shirt', 1, 499.00, 499.00, '2025-09-22 13:39:11'),
-(6, 19, 50, 24, 'Olive Green Shirt', 1, 499.00, 499.00, '2025-09-22 13:39:11');
+(6, 19, 50, 24, 'Olive Green Shirt', 1, 499.00, 499.00, '2025-09-22 13:39:11'),
+(7, 19, 57, 26, 'Maroon Shirt', 1, 499.00, 499.00, '2025-09-24 11:49:35'),
+(8, 19, 57, 30, 'Navy Blue Shirt', 1, 499.00, 499.00, '2025-09-24 11:49:35'),
+(10, 19, 67, 27, 'Red Shirt', 2, 499.00, 998.00, '2025-09-24 14:42:30');
 
 -- --------------------------------------------------------
 
@@ -275,7 +288,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `address`, `username`, `password`, `is_admin`, `created_at`) VALUES
-(2, 'jaymar', '', NULL, 'admin44', 'scrypt:32768:8:1$G6jR3JvpRuCHlucr$aac5963ebc4b5fabe2a971af9aaf54c41f7e44691c838ac7014ae2a1750ad4e0cf168bccadc00271c91d1546ad1057b325504e588f8ef390ba89d4f8dde38834', 'admin', '2025-08-20 02:53:24'),
+(2, 'jaymar', 'Roco', 'Happyland St.', 'admin44@admin44', 'scrypt:32768:8:1$VGEhaZSH5d82U3Nw$9b62c86aab50509add040ae3f2880014a24728dd7bdfbe816f7ceab2207d2b0d7be94a29f0f68424d43f5b40ed75ae7dc6cf1c881a887cb5657a701b3eafc376', 'admin', '2025-08-20 02:53:24'),
 (4, 'admin23', 'admin23', NULL, 'admin23@admin23', 'scrypt:32768:8:1$tqS5ZPYsPW7Sye9B$c0ebeaf8e808414441274fcf2cd7460d857e6029d734cd604d5c1e38feb49277606199e74732a24563c45555fb557ffda51b3a2c994250a6da19bc596c207652', 'user', '2025-08-20 04:49:31'),
 (7, 'asdasd', 'adsad', NULL, 'adsdasd@asd', 'scrypt:32768:8:1$THcQ502TD6ZLAMWL$d830e861811aaa76c501b63efecc43df179c4bbee89a66d283f8f80fee3dfddc1833007f763a58c2605eb112d975d59670c5a4852cfb0635d36d5eaec7ad8d35', 'user', '2025-08-20 23:16:36'),
 (9, 'asdasd', 'adsad', NULL, 'adsdasd@asdsssss', 'scrypt:32768:8:1$eeW4wNUaaTKBwHkg$409d347da0220fea476c0513c98571bdde202cc49348401ec9fdf06edd438cce0c7219b69591036234b918b7813bc11d156400b523e5d8e23ab63d202ef63837', 'admin', '2025-08-20 23:20:59'),
@@ -285,7 +298,8 @@ INSERT INTO `users` (`id`, `firstname`, `lastname`, `address`, `username`, `pass
 (13, 'client', 'client', NULL, 'client@client', 'scrypt:32768:8:1$6i3Rz5JdhfksmmZ4$8cf6afd82036ba52e4d21917abebc51a1a88f416f94d6c74bd30af8a86b95ad8c976865cab8d1b5d8e36b5c6adc5c2b5510bd6219c35dbc443b83cd385830bbc', 'user', '2025-08-27 01:27:10'),
 (17, 'Client 1', 'client@23', 'Litex Rd. Commonwealth, Quezon City', 'client@23', 'scrypt:32768:8:1$X1RsmagYy4jBfyZl$cd376fabf94e3bf098a9ac788cc8c77b747b2502952396109f1cbfdf1a86df6b5adc3956631aa00b30bf51761ba41f8816990230863ffd3339828545fa212040', 'user', '2025-08-31 12:17:39'),
 (18, 'client16', 'client16', 'Happyland St.', 'client@16', 'scrypt:32768:8:1$FYp8ZYyy96hWlYj7$ae2095291e994505a69c6e86cc43f2acf87afe354b9f75c214f4a675b3e4df076be53a9e7462e5d7000a908b009f472ebb181abbc3972b688f8622e0e570e590', 'user', '2025-09-01 10:44:11'),
-(19, 'client@1', 'client@1', 'Litex Rd. Commonwealth, Quezon City', 'client@1', 'scrypt:32768:8:1$SjL9gliB3CBKqqaU$5ad8d9cc4ee7880724b5970c9c7ee9d6499a292d1e759700d74adc153bbb292f5d60d13ce7c1c013187e224f1acbe53e5464f33f96b7a6a0764421c4ec48abd9', 'user', '2025-09-22 05:37:49');
+(19, 'client@1', 'client@1', 'Litex Rd. Commonwealth, Quezon City', 'client@1', 'scrypt:32768:8:1$SjL9gliB3CBKqqaU$5ad8d9cc4ee7880724b5970c9c7ee9d6499a292d1e759700d74adc153bbb292f5d60d13ce7c1c013187e224f1acbe53e5464f33f96b7a6a0764421c4ec48abd9', 'user', '2025-09-22 05:37:49'),
+(20, 'asda', 'sdasd', NULL, 'asdas@asdasd', 'scrypt:32768:8:1$c1yVY0PngUfsasky$10d8f5a13dce53e3f10c9805899bc176f994815e3eeec2cf92a6b75101b6eece42d5fd19934cc3f36cad4293e9aa09d3687881df57fbe7d06e0ed83b4f5231ea', 'user', '2025-09-22 08:47:00');
 
 --
 -- Indexes for dumped tables
@@ -351,43 +365,43 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=180;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=221;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
 
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=160;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=196;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `product_variants`
 --
 ALTER TABLE `product_variants`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
 
 --
 -- AUTO_INCREMENT for table `purchase_history`
 --
 ALTER TABLE `purchase_history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- Constraints for dumped tables
